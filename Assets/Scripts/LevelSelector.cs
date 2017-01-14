c8bcbe4cde8fdcc75f478e6075bc72424fd0d858
Christopher Miller <admin@mctherealm.net>
2017-01-13 19:27:09 -0500

    Fixed Up Main Menu

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -2,32 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using VRTK;
 
-[RequireComponent(typeof(SteamVR_TrackedObject))]
-public class LevelSelector : MonoBehaviour {
+public class LevelSelector : VRTK_InteractableObject
+{
 
-    SteamVR_TrackedObject trackedObj;
-    public float selectDistance = 0.5f;
+    public string levelToLoad;
 
-    // Use this for initialization
-    void Awake () {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate() {
-        var device = SteamVR_Controller.Input((int)trackedObj.index);
-        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
-        {
-            foreach(GameObject obj in GameObject.FindGameObjectsWithTag("LevelSelect"))
-            {
-                Debug.Log(Vector3.Distance(gameObject.transform.position, obj.transform.position));
-                if (Vector3.Distance(gameObject.transform.position, obj.transform.position) < selectDistance)
-                {
-                    SteamVR_Fade.View(Color.white, 1);
-                    SceneManager.LoadScene(obj.name);
-                }
-            }
-        }
+    public override void StartUsing(GameObject usingObject)
+    {
+        base.StartUsing(usingObject);
+        SceneManager.LoadScene(levelToLoad);
     }
 }
