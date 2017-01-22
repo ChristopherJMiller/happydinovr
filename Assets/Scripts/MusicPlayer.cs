945ba99a5d6dd5cfef665ad2f9ae6cfc3abf9995
Christopher Miller <admin@mctherealm.net>
2017-01-22 11:43:45 -0500

    Added music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+
+    public AudioClip[] tracks;
+    public bool random = false;
+    private int currentTrack = 0;
+    private AudioSource localAudioSource;
+
+    void Start()
+    {
+        localAudioSource = GetComponent<AudioSource>();
+        Random.InitState(Mathf.RoundToInt(Time.time * 10000000));
+    }
+
+    void Awake () {
+        Random.InitState(Mathf.RoundToInt(Time.time * 10000000));
+	}
+
+    void FixedUpdate()
+    {
+        if (!localAudioSource.isPlaying)
+        {
+            if (random)
+            {
+                localAudioSource.PlayOneShot(tracks[Random.Range(0, tracks.Length - 1)]);
+            }
+            else
+            {
+                localAudioSource.PlayOneShot(tracks[currentTrack]);
+                currentTrack++;
+                if (currentTrack == tracks.Length)
+                {
+                    currentTrack = 0;
+                }
+            }
+        }
+    }
+}
